a90cd42d5e3dd153a576190c0d746f3ce47586c7
Matthew Beckerleg <ord.sc2@gmail.com>
2020-01-10 10:23:57 -0800

    new 'query all' promise structure

--- server/queryAll.js
+++ server/queryAll.js
@@ -1,79 +1,29 @@
-const express = require('express');const cors = require('cors');
-const path = require('path');
-const rp = require('request-promise');
 const ip = require('./ip.js');
-const proxy = require('http-proxy-middleware');
-const app = express();
 
-let photos_q = () => {
-  app.get('/photos/:id', (req, res) => {
-    const { id } = req.params;
-    res.redirect(`http://${ip.photos}/photos/${id}`);
-  });
-};
+const qphotos = `http://${ip.photos}/photos/`;
+const qinfo = `http://${ip.info}/listings/`;
+const qbooking = `http://${ip.booking}/booking`;
+const qroom = `http://${ip.rooms}/room`;
+const qreviews = `http://${ip.reviews}/reviews/${id}`;
+const qmorehomes = `http://${ip.morehomes}/MoreHomes/?id=`;
 
-let listings_q = () => {
-  app.get('/listings/:id', (req, res) => {
-    const { id } = req.params;
-    res.redirect(`http://${ip.info}/listings/${id}`);
-  });
-};
+const batchOne = [qphotos, qinfo, qreviews]
+const batchTwo = [qbooking, qroom, qmorehomes]
 
-let booking_q = () => {
-  app.use('/booking',
-    proxy({
-      target: `http://${ip.booking}/booking`,
-      pathRewrite: (path, req) => {
-        var querystring = '?';
-        for (key in req.query) {
-          if (querystring !== '?') {
-            querystring += '&';
-          }
-          querystring += `${ key }=${ req.query[key] }`;
-        }
-        return querystring;
-      }
-    })
-  );
-};
-
-let rooms_q = () => {
-  app.use('/room',
-    proxy({
-      target: `http://${ip.rooms}/room`,
-      pathRewrite: (path, req) => {
-        var querystring = '?';
-        for (key in req.query) {
-          if (querystring !== '?') {
-            querystring += '&';
-          }
-          querystring += `${ key }=${ req.query[key] }`;
-        }
-        return querystring;
-      }
-    })
-  );
-};
-
-let reviews_q = () => {
-  app.get('/reviews/:id/', (req, res) => {
-    const { id } = req.params;
-    res.redirect(`http://${ip.reviews}/reviews/${id}`);
-  });
-};
-
-let morehomes_q = () => {app.route('/MoreHomes')
-  .get((req, res) => {
-    rp(`http://${ip.morehomes}/MoreHomes/?id=`)
-      .then((body) => res.send(body))
-      .catch(() => res.sendStatus(500))
-  })
-};
-
-const allQs = Promise.all([photos_q()]) 
-  // listings_q, booking_q, rooms_q, reviews_q, morehomes_q])
-
-//  .then(body => console.log(body))
-//  .catch((err) => console.log( 'Promise all error: ', err))
-
-module.exports = allQs;
+module.exports.allQs = (req, res) => {
+  const { id } = req.params;
+  const resp = [];
+  Promise.all([
+    Promise.all(batchOne.map(url =>
+      rp(`${url}/${id}`)
+      .then(data => resp.push(data))
+      .catch(err => resp.push(data))
+    )),
+    Promis.all(batchTwo.map(url => 
+      rp(`${url}/${id}`)
+      .then(data => resp.push(data))
+      .catch(err => resp.push(err))
+    ))
+  ])
+  .then((resp) => res.send(resp))
+}
